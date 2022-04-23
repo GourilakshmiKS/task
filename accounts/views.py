18ef3a2095ed4c75d955f54b85f12ee6728d05cb
GourilakshmiKS.com <gourilakshmiks@gmail.com>
2022-04-23 09:51:43 +0530

    creating customer prfiles

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,57 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import *
-from .forms import OrderForm
-from .filters import OrderFilter
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from .models import *
+from .forms import OrderForm,CreateUserForm
+from .filters import OrderFilter
+from .decorators import unauthenticated_user, allowed_users, admin_only
+@unauthenticated_user
+def registerPage(request):
+    
+    form=CreateUserForm()
+
+    if request.method=="POST":
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            username=form.cleaned_data.get('username')
+            
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+    
+  
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
+@unauthenticated_user
+def loginPage(request):
+    '''if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:    '''
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.info(request,'Username OR password is incorrect')    
+                    
+    context = {}
+    return render(request,'accounts/login.html',context)    
+        
+ 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login') 
+@allowed_users(allowed_roles=['admin'])  
 def home(request):
     orders=Order.objects.all()
     customer=Custom.objects.all()
@@ -14,12 +62,47 @@ def home(request):
     pending=orders.filter(status='Pending').count()
 
     context={'orders':orders,'customer':customer,'total_orders':total_orders,'delivered':delivered,'pending':pending}
-    return render(request,'accounts/dashboard.html',context)
+    return render(request, 'accounts/dashboard.html', context)
 
+
+@login_required(login_url='login')  
+@allowed_users(allowed_roles=['customer']) 
+def userPage(request):
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    print('ORDERS:', orders)
+
+    context = {'orders':orders, 'total_orders':total_orders,'delivered':delivered,'pending':pending}
+    return render(request,'accounts/user.html',context)
+
+ 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+	customer = request.user.customer
+	form = CustomerForm(instance=customer)
+
+	if request.method == 'POST':
+		form = CustomerForm(request.POST, request.FILES,instance=customer)
+		if form.is_valid():
+			form.save()
+
+
+	context = {'form':form}
+	return render(request, 'accounts/account_settings.html', context)
+
+   
+
+
+@login_required(login_url='login')  
+@allowed_users(allowed_roles=['admin']) 
 def products(request):
     products=Product.objects.all()
     return render(request,'accounts/products.html',{'products':products})
 
+@login_required(login_url='login')  
+@allowed_users(allowed_roles=['admin']) 
 def customer(request,pk_test):
     customer=Custom.objects.get(id=pk_test)
     orders=customer.order_set.all()
@@ -30,7 +113,8 @@ def customer(request,pk_test):
 
     context={'customer':customer,'orders':orders,'order_count':order_count,'myFilter':myFilter}
     return render(request,'accounts/customer.html',context)   
-
+@login_required(login_url='login')   
+@allowed_users(allowed_roles=['admin']) 
 def createOrder(request):
     form=OrderForm()
     if request.method=='POST':
@@ -42,7 +126,7 @@ def createOrder(request):
     context= {'form':form}
     return render(request,'accounts/order_form.html',context)        
 
-
+@login_required(login_url='login')   
 def updateOrder(request,pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -55,7 +139,7 @@ def updateOrder(request,pk):
     
     context= {'form':form}
     return render(request,'accounts/order_form.html',context)
-
+@login_required(login_url='login')   
 def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
     if request.method=="POST":
--- task/settings.py
+++ task/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'accounts.apps.AccountConfig',
     'django_filters',
 ]
 
@@ -125,8 +125,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
